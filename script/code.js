00e7d3fdafc6cc6fa333f11edffe3c89787ba62c
nathaliewillemse21 <njpwillemse69@gmail.com>
2023-10-10 10:58:45 +0200

    Updates

---
+++ script/code.js
@@ -0,0 +1,6 @@
+let firstName = 'Nathalie'
+console.log("My name is "+ firstName)
+console.log(firstName +" have 2 siblings")
+
+console.log(`My name is ${firstName}`);
+console.log(`${firstName} have 2 siblings`)
